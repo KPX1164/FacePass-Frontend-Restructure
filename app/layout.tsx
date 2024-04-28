040b44d75f5f53c1b460217562c6115204dd7827
Hakushin <kpx1164@gmail.com>
2024-04-28 22:02:59 +0700

    new layout classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import { useRouter } from "next/navigation";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
-// import { metadata } from "./metadata"; // Import metadata from the separate file
-
+import useToken from "@/hooks/useToken";
+import Navigation from "@/components/Navigation";
 const inter = Inter({ subsets: ["latin"] });
-
+import Footer from '@/components/Footer'
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,44 +17,10 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="HStack w-full items-center justify-center">
-          <header className="w-full HStack items-center justify-between p-5">
-            <div className="HStack gap-5 text-sm items-center">
-              <div
-                className="HStack gap-2 cursor-pointer"
-                onClick={() => router.push("/")}
-              >
-                <p className="font-semibold text-lg">Developer.FacePass</p>
-              </div>
-              <p className=" cursor-pointer" onClick={() => router.push("/")}>
-                Home
-              </p>
-              <p
-                className=" cursor-pointer"
-                onClick={() => router.push("/plugin")}
-              >
-                Plugin
-              </p>
-              <p
-                className=" cursor-pointer"
-                onClick={() => router.push("/pricing")}
-              >
-                Pricing
-              </p>
-            </div>
-            <div className="HStack gap-5">
-              <p
-                className=" cursor-pointer flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-                onClick={() => router.push("/sign-up")}
-              >
-                Developer account{" "}
-              </p>
-              
-            </div>
-          </header>
-        </div>
+<body className={inter.className + " text-foreground bg-background"}>
+        <Navigation />
         {children}
+        {/* <Footer /> */}
       </body>
     </html>
   );
