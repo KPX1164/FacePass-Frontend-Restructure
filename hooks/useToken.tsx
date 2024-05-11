431e96634b62493754e5de9674e8fbc13d45fa51
deityzs <kongkonghihi@gmail.com>
2024-05-12 00:51:14 +0700

    getRole in useToken

--- hooks/useToken.tsx
+++ hooks/useToken.tsx
@@ -28,16 +28,27 @@ function useToken() {
     return null;
   }
 
+  const getRole = () => {
+    if (typeof window !== 'undefined') {
+      const tokenString = localStorage.getItem('token');
+      const userToken = JSON.parse(tokenString);
+      return userToken?.user.role;
+    }
+    return null;
+  }
+
   const [token, setToken] = useState(getToken());
   const [username, setUsername] = useState(getFirstName());
   const [userId, setUserId] = useState(getID());
+  const [role, setRole] = useState(getRole());
 
 
   const saveToken = (userToken:any) => {
     localStorage.setItem('token', JSON.stringify(userToken));
-    setUserId(userToken.id);
+    setUserId(userToken.user.id);
     setToken(userToken.token);
     setUsername(userToken.user.first_name);
+    setRole(userToken.user.role);
   };
 
   const logout = () => {
@@ -54,7 +65,8 @@ function useToken() {
     isLoggedIn: !!token,
     logout,
     username,
-    userId
+    userId,
+    role
   };
 }
 
