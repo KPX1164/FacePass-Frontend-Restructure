f7dfe08948c49a05eeb6fdf552f2876530c561a9
Hakushin <kpx1164@gmail.com>
2024-05-19 17:57:22 +0700

    update demo

--- app/developer/demo/page.tsx
+++ app/developer/demo/page.tsx
@@ -1,10 +1,37 @@
-import React from "react";
-import { Input } from "@nextui-org/react";
+"use client";
+import React, { useState } from "react";
+import FacePass from "@/npm-public/FacePass";
 
 export default function App() {
+  const defaultCode = `import React from "react";
+import FacePass from '@/npm-public/FacePass'
+export default function App() {
+  const requests = ["fullname", "phone","email","location"]
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Demo</p>
+      <FacePass scopes={requests}/>
+  );
+}
+  `;
+  const [code, setCode] = useState(defaultCode);
+  const requests = ["fullname", "phone", "email", "location"];
+
+  return (
+    <main className="flex min-h-screen top-0 fixed w-screen">
+      <section className=" w-full mt-20 mb-10">
+        <div className="flex w-full h-full">
+          <section className="w-1/2 h-full border-gray-500 border-2 border-opacity-25 bg-white rounded-lg p-4 mr-2">
+            <textarea
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              className="w-full h-full"
+            />
+          </section>
+          <section className="w-1/2 h-full p-4 ml-2 VStack items-center justify-center">
+            {/* <iframe srcDoc={code} title="preview" className="w-full h-full" /> */}
+            <FacePass scopes={requests} />
+          </section>
+        </div>
+      </section>
     </main>
   );
 }
