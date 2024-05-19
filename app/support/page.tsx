a36057c066db31197f3a9037c5e888600bc1e2a8
Hakushin <kpx1164@gmail.com>
2024-05-20 02:31:21 +0700

    update support page

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,20 +1,8 @@
 "use client";
 import React from "react";
-import { Link } from "@nextui-org/react";
-import { GoArrowUpRight } from "react-icons/go";
-import { Input } from "@nextui-org/react";
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  useDisclosure,
-} from "@nextui-org/react";
-import SupportForm from "@/components/SupportForm";
+
+import SupportContact from "@/components/SupportContact";
 export default function App() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
     <main className="flex min-h-screen flex-col justify-between lg:items-center">
@@ -29,40 +17,9 @@ export default function App() {
 
         <div className="mt-5"></div>
 
-        <section className="VStack items-center p-10 pt-12 gap-4 text-center h-[600px] bg-white rounded-lg shadow-sm relative overflow-hidden">
-          <div className="gap-4 VStack items-center z-10">
-            <p className="font-medium text-3xl">Get Support</p>
-            <p className="opacity-70">
-              Give us a few details and we’ll offer the best solution. Connect
-              by phone, email, and more.
-            </p>
-            <Button
-              radius="full"
-              className="bg-black text-white w-fit shadow-lg"
-              onPress={onOpen}
-            >
-              Start now
-            </Button>
-          </div>
-          <img
-            src="support.jpg"
-            className="absolute bottom-0 left-0 w-full h-full max-h-[400px] object-cover z-0"
-            alt="Support"
-          />
-        </section>
+       <SupportContact/>
       </section>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <section className="w-full h-full VStack items-center gap-5 p-5 pt-10">
-                <p className="font-medium text-2xl">Get in touch</p>
-                <SupportForm />
-              </section>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+      
     </main>
   );
 }
