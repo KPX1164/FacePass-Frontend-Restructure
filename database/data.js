1e49b1010abd3a89e978c2d7a1cc014dccf4eb6b
Hakushin <kpx1164@gmail.com>
2024-05-26 19:16:47 +0700

    add mock data

--- database/data.js
+++ database/data.js
@@ -1,7 +1,6 @@
 import React from "react";
 const columns = [
   {name: "NAME", uid: "name"},
-  {name: "ROLE", uid: "role"},
   {name: "STATUS", uid: "status"},
   {name: "ACTIONS", uid: "actions"},
 ];
